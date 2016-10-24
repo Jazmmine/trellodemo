be325b4f95c0877a1f6919e46f0230669e2140b2
Jazmine Angoma <jazmine.angoma@gmail.com>
2016-10-23 22:11:51 -0500

    subiendo textotitulo y span tarjeta

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function () {
-	var contentTabla = document.getElementById("contenido-tabla");
+	var contentTabla = document.getElementById("content-tabla");
 	var contentFormularioTitulo = document.getElementById("content-formulario-titulo");
 	var formularioTitulo = document.getElementById("formulario-titulo");
 	var spanTitulo = document.getElementById("span-titulo");
@@ -13,6 +13,7 @@ window.addEventListener("load", function () {
 			var inputTitulo = document.createElement("input");
 				inputTitulo.setAttribute("type","text");
 				inputTitulo.setAttribute("id","input-titulo");
+				inputTitulo.classList.add("bloque");
 				inputTitulo.setAttribute("placeholder", "Añadir una lista...");
 				divTitulo.appendChild(inputTitulo);
 				inputTitulo.focus();
@@ -20,7 +21,7 @@ window.addEventListener("load", function () {
 				btnTitulo.classList.add("btn");
 				btnTitulo.textContent = "Guardar";
 				divTitulo.appendChild(btnTitulo);
-				//btnTitulo.addEventListener("click", creandoLista);
+				btnTitulo.addEventListener("click", creandoTitulo);
 			var cerrarTitulo = document.createElement("a");
 				cerrarTitulo.classList.add("btn-floating");
 				divTitulo.appendChild(cerrarTitulo);
@@ -28,6 +29,29 @@ window.addEventListener("load", function () {
 				icoCerrarTitulo.classList.add("material-icons");
 				icoCerrarTitulo.textContent = "cancel";
 				cerrarTitulo.appendChild(icoCerrarTitulo);
+		}
+
+		function creandoTitulo(evento){
+			var contenedorColumna = this.parentElement.parentElement.parentElement.parentElement.parentElement;
+			var inputTitulo = document.getElementById("input-titulo");
+			var inputTituloValor = inputTitulo.value;
+				inputTitulo.value = "";
+			var titulo = document.createElement("div");
+				titulo.innerHTML = "<b>" + inputTituloValor + "</b>";
+				contenedorColumna.appendChild(titulo);
+			var divTarjeta = document.createElement("div");
+				contenedorColumna.appendChild(divTarjeta);
+			var divTarjetaSpan = document.createElement("div");	
+				divTarjeta.appendChild(divTarjetaSpan);			
+			var tarjetaSpan = document.createElement("span");
+				tarjetaSpan.textContent = "Añadir una tarjeta...";
+				divTarjetaSpan.appendChild(tarjetaSpan);
+				//tarjetaSpan.addEventListener("click",creandoFormTarjeta);
+			var creandoColumna = document.createElement("div");
+				creandoColumna.classList.add("class","columna");
+				contentTabla.appendChild(creandoColumna);
+			var hijoNuevaColumna = this.parentElement.parentElement.parentElement.parentElement
+				creandoColumna.appendChild(hijoNuevaColumna);
 		}
 
 });
